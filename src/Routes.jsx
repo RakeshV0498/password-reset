42b8614de0923a9eb961ac05681486300c8528bb
RakeshV0498 <rockr4674@gmail.com>
2024-06-17 08:10:21 +0530

    Updated Primary route

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,11 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import MyNavbar from "./Pages/Navbar";
+import Footer from "./Pages/Footer";
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
+      <MyNavbar />
       <Routes>
-        <Route path="/" element={<h1>Hi There</h1>} />
+        <Route path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 };
