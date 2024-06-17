004ad507a40080a142ecd15870de6e255a5784eb
RakeshV0498 <rockr4674@gmail.com>
2024-06-17 07:46:34 +0530

    Implemented Route Logic

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
-import AppRoutes from "./Routes.js";
+import AppRoutes from "./Routes.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
