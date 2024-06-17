5a838f285722d3d860d77109a38f77d6677417a3
RakeshV0498 <rockr4674@gmail.com>
2024-06-17 08:52:08 +0530

    Updated Navbar componet styles

--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -1,23 +1,28 @@
-import { Navbar, Nav, Button, Container } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 import { FaSignInAlt, FaUserPlus } from "react-icons/fa";
+import shopIcon from "../assets/store-shopper.svg";
+import { Link } from "react-router-dom";
 
-const MyNavbar = () => {
+function MyNavbar() {
   return (
     <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">
+      <Container fluid>
+        <Navbar.Brand as={Link} to="/" className="d-flex align-items-center">
           <img
-            src="path/to/your/logo.png"
+            src={shopIcon}
             width="30"
             height="30"
-            className="d-inline-block align-top"
+            className="d-inline-block align-top mr-2"
             alt="Logo"
           />
-          {" My Website"}
+          Flipzone
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end ">
+          <Nav className="ml-auto gap-3">
             <Button variant="outline-primary" className="mr-2">
               <FaSignInAlt className="mr-1" />
               Login
@@ -31,6 +36,6 @@ const MyNavbar = () => {
       </Container>
     </Navbar>
   );
-};
+}
 
 export default MyNavbar;
