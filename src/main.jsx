3d8b7a5106828eefb8f080ac41f268724cefbdee
RakeshV0498 <rockr4674@gmail.com>
2024-06-17 07:45:54 +0530

    Updated File extensions

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
+import AppRoutes from "./Routes.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <AppRoutes />
   </React.StrictMode>
 );
