80ec36d0e86684d97c65de8ac6d0da0a47fcb0b0
RakeshV0498 <rockr4674@gmail.com>
2024-06-17 09:44:44 +0530

    Implemented logic to get data

--- src/Pages/Forms/ForgotPassword.jsx
+++ src/Pages/Forms/ForgotPassword.jsx
@@ -2,8 +2,17 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { FaEnvelope } from "react-icons/fa";
 import Container from "react-bootstrap/Container";
+import { useState } from "react";
 
 function ForgotPassword() {
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email);
+    setEmail("");
+  };
+
   return (
     <Container
       className="d-flex justify-content-center align-items-center"
@@ -11,13 +20,21 @@ function ForgotPassword() {
     >
       <div style={{ maxWidth: "400px", width: "100%" }}>
         <h2 className="text-center mb-4">Forgot Password</h2>
-        <Form>
+        <Form onSubmit={handleSubmit}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <div className="input-group mb-3">
               <span className="input-group-text">
                 <FaEnvelope />
               </span>
-              <Form.Control type="email" placeholder="Enter your email" />
+              <Form.Control
+                type="email"
+                placeholder="Enter your email"
+                name="email"
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
             </div>
           </Form.Group>
           <Button variant="primary" type="submit" className="w-100">
